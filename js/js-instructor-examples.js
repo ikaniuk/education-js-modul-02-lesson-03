fded7210b049f123fa8c38aba6d6a35c1a917c95
Ihor Kaniuk <ikanyuk@icloud.com>
2023-01-20 18:38:31 +0200

    training

---
+++ js/js-instructor-examples.js
@@ -0,0 +1,33 @@
+// const fruits = ['Mango', 'Papaya', 'Watermelon', 'Apple', 'Coconut'];
+
+// for (let i = 0; i < fruits.length - 1; i += 1) {
+//     console.log(fruits[i]);
+// }
+
+// for (const fruit of fruits) {
+//     console.log(fruit);
+// }
+
+
+// const string = 'Mango';
+
+// console.log(string.split(''));
+// const array = string.split('');
+// console.log(array.join(''));
+
+
+
+// const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+// console.log(clients.indexOf("Poly")); // 2
+// console.log(clients.indexOf("Monkong")); // -1
+
+
+// Выносим варианты в массив
+// const redFruits = ["apple", "strawberry", "cherry", "cranberries"];
+// const fruit = "cherry";
+// // Проверяем наличие элемента
+// const hasFruit = redFruits.includes(fruit);
+
+// if (hasFruit) {
+//   console.log(`${fruit} is a red fruit!`);
+// }
