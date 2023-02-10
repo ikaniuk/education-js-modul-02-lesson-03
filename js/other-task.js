fd3fde8a58f72d69e34473e96548669547dd9572
Ihor Kaniuk <ikanyuk@icloud.com>
2023-02-10 22:01:57 +0200

    complete-task-08
    complete-task-08-other-task-arrays

--- js/other-task.js
+++ js/other-task.js
@@ -121,7 +121,77 @@
 // -------------------------Task-06-----------------------
 
 // Задание:
-// Дано массив.Поменяйте элемент с максимальным значением местами с элементом с 
+// Дано массив.Поменяйте элемент с максимальным значением местами с элементом с
 // минимальным значением.
 
-// const values = [10, 20, 4, 6, 11, 9, 125, 1, 8, 0, 3];
+// const values = [10, 20, 4, 6, 11, 9, 125, 1, 8, 0];
+
+// let maxNumber = values[0];
+// let minNumber = values[0];
+// let maxElementIndex;
+// let minElementindex;
+
+// for (let i = 0; i < values.length; i += 1){
+//     console.log(values[i])
+//     if (values[i] > maxNumber) {
+//         maxNumber = values[i];
+//         maxElementIndex = [i]; // запоминаем индекс максимального значения
+//     }
+
+//     if (values[i] < minNumber) {
+//         minNumber = values[i];
+//         minElementindex = [i]; // запоминаем индекс минимального значения
+//     }
+// }
+
+// // меняем значения местами, используя индексы, которые запомнили ранее
+// values[maxElementIndex] = minNumber;
+// values[minElementindex] = maxNumber;
+
+// console.log('----------------------------------------')
+
+// for (const value of values) {
+//     console.log(value);
+// }
+
+
+// -------------------------Task-07---------------------
+
+// Задание:
+// Дано массив. Найдите среднее арифметическое для значений этого массива.
+// Выведите на экран только те элементы, которые больше этого среднего арифметического
+// значения
+
+// let values = [10, 20, 48, 6, 11, 9, 125, 1, 8, 0, 3, 255, 77, 11, 170, 4, 28];
+// let sum = 0;
+
+// console.log(values);
+
+// for (const value of values) {
+//     sum += value;
+// }
+
+// const avarage = Math.floor(sum / values.length);
+// console.log('Среднее арифметическое =', avarage);
+
+// for (const value of values) {
+//     if (value > avarage) {
+//         console.log(value);
+//     }
+// }
+
+
+// -------------------------Task-08-------------------------------
+// Задание:
+// Дан массив целых чисел.Проверьте есть ли в нем одинаковые элементы. 
+// Если есть выведите их на экран.
+
+// let values = [10, 20, 4, 6, 11, 9, 125, 10, 8, 0, 3, 5, 89, 12, 46, 77, 3];
+
+// for (let i = 0; i < values.length; i += 1){
+//     for (let j = i + 1; j < values.length; j += 1){
+//         if (values[i] === values[j]) {
+//             console.log(values[i])
+//         }
+//     }   
+// }
