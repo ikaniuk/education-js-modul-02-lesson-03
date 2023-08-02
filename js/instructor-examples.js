30650ec5911c550ef4191baa29adf468ced19455
Ihor Kaniuk <ikanyuk@icloud.com>
2023-08-02 22:10:29 +0300

    update

---
+++ js/instructor-examples.js
@@ -0,0 +1,24 @@
+// -Модуль 2. Занятие 3. Массивы-
+
+// Example 1 - Базовые операции с массивом
+// Создайте массив genres с элементами «Jazz» и «Blues».
+// Добавьте «Рок-н-ролл» в конец.
+// Выведите в консоль первый элемент массива.
+// Выведите в консоль последний элемент массива. Код должен работать 
+// для массива произвольной длины.
+// Удалите первый элемент и выведите его в консоль.
+// Вставьте «Country» и «Reggae» в начало массива.
+
+// const genreOfMusic = ['Jazz', 'Blues'];
+// console.log(genreOfMusic);
+
+// genreOfMusic.push('Рок-н-ролл');
+// console.log(genreOfMusic)
+// console.log(genreOfMusic[0]);
+
+// const indexOfLastElement = genreOfMusic.length - 1;
+
+// console.log(genreOfMusic[indexOfLastElement]);
+
+// const firstElement = genreOfMusic.slice(1);
+// console.log(firstElement);
